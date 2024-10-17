d91a7a4f1dcf89d5b6bd869b31eaa407c0e8d2e6
NachoRodriguez97 <71531765+NachoRodriguez97@users.noreply.github.com>
2024-10-17 12:02:32 -0300

    Update UseApiContext.jsx

--- src/hooks/UseApiContext.jsx
+++ src/hooks/UseApiContext.jsx
@@ -4,14 +4,8 @@ const ApiContext = createContext();
 
 const ApiProvider = ({ children }) => {
     const [apiUrls, setApiUrls] = useState({
-        //loginApi: 'http://localhost:23773/Login/',
         loginApi: 'http://app-web-test:5560/Login/',
-        //loginApi: 'https://apitest-loggrouper.loginter.com.ar/Login/',
-        //loginApi: 'http://app-web-01:2010/Login/',
-        //logGrouperApi:'http://localhost:23773/Grouper/',
         logGrouperApi:'http://app-web-test:5560/Grouper/',
-        //logGrouperApi: 'https://apitest-loggrouper.loginter.com.ar/Grouper/',
-        //logGrouperApi:'http://app-web-01:2010/Grouper/'
     });
 
     return (
